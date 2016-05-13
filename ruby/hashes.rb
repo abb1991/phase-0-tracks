7a58310859e39a03928a04a8e29203ef04fa21f5
abb1991 <alexbills1991@gmail.com>
2016-05-13 10:36:07 -0700

    Initial commit, added pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+#"Hello,thanks for choosing 'Interior Decliners' for your home
+# decoration needs. Here everything is too good for you!"
+#"Please tell us a little bit about you and your perfectly adequate, but # not-quite-good-enough for our services family:"
+# Ask for client's name, age, proposed theme, fears, children
+# "Thanks for the info, I'll see if I there's anything we can do to meet your needs"
+"Could you check and make sure these are really the answers you want to give? (y/n)"
+  - if yes "Oh thank you! I was worried for a minute. Why don't you re-enter your info."
+  - once entered "That'll have to do I suppose..."
+-print info "After reviewing your information, we here at 'Interior Decliners' are unable to move forward. I can't believe you thought #{theme} was a good idea. Ta ta!"
+
