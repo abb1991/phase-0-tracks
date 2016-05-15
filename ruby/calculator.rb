f8e273764f13b6da2ca530fdba1f9c376473253b
abb1991 <alexbills1991@gmail.com>
2016-05-15 15:29:03 -0700

    passd user input to parameter of calculator method.

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,22 +1,23 @@
 def calculator(integer_1, function, integer_2)
 operator = function.to_sym
 
-if operator == :+
-  p integer_1 + integer_2
-elsif operator == :-
- p integer_1 - integer_2
-elsif operator == :*
-  p integer_1 * integer_2
-else operator == :/
-  p integer_1 / integer_2
+    if operator == :+
+      p integer_1 + integer_2
+    elsif operator == :-
+     p integer_1 - integer_2
+    elsif operator == :*
+      p integer_1 * integer_2
+    else operator == :/
+      p integer_1 / integer_2
+    end
 end
-p operator
-end
-
-# calculator(10, "+", 2)
 
 puts "Please type in a simple computation (+,-,*,/), for example 1+2:"
 user_input = gets.chomp
 user_array = user_input.split(" ")
 
-p user_array
+num_1 = user_array[0].to_i
+user_operator = user_array[1]
+num_2 = user_array[2].to_i
+
+calculator(num_1, user_operator, num_2)
