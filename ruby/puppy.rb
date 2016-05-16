2b6dd6926cbdad1ce4c60252977d86608f22e6bc
abb1991 <alexbills1991@gmail.com>
2016-05-16 15:24:57 -0700

    initial commit, added comments for Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Animal--------------
+# Puppy
+
+# Characteristics ------
+# Energy: unlimited
+# cuteness: 10
+# bark: quiet
+# size: tiny
+# color: varies
+
+# Behavior-------
+# Chases tail
+# rolls on back
+# sleeps on mom's tummy
+# befriends other tiny creatues
