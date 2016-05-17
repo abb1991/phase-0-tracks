82cce7191dce7b3344375911dd34c882985285d5
abb1991 <alexbills1991@gmail.com>
2016-05-16 18:31:42 -0700

    initial commit, copied code from pairing session in coder pad

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,76 @@
 class Puppy
 
+  def initialize
+    p "initializing new Puppy instance... "
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
+
+  end
+
+  def speak(i)
+    p "Woof!" * i
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(i)
+    p "I'm #{i * 7} years old! - dog"
+  end
+
+  def file_taxes(date_filed)
+    p "I filed your taxes on #{date_filed}!"
   end
 
 end
 
+scruffy = Puppy.new
+scruffy.fetch("ball")
+scruffy.speak(10)
+scruffy.roll_over
+scruffy.dog_years(3)
+scruffy.file_taxes("April 15th")
+
+
+class Sitcoms
+
+  def initialize
+    p "initializing sitcom..... (laugh track)"
+  end
+
+  def seinfeld(thought)
+    p "#{thought} ....and yada yada yada."
+  end
+
+  def frasier(name)
+    p "(in a baritone) Thank you #{name} for calling in, I'm         listening..."
+  end
+
+end
+
+viewer = Sitcoms.new
+viewer.seinfeld("I became a coder")
+viewer.frasier("Sierra")
+
+arr = []
+
+50.times do |i|
+  i = Sitcoms.new
+  arr.push(i)
+end
+
+arr.each do |i|
+  i.seinfeld("test")
+  puts "\n"
+  i.frasier("Randolph")
+  puts "\n"
+end
+
+
+
+
+p arr
