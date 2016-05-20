5a17b1dc6fd7068247a6a18ca1ad32338c4bc744
abb1991 <alexbills1991@gmail.com>
2016-05-20 15:56:52 -0500

    added initialize, add_population, add_country methods, and random population generator.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,12 +1,36 @@
 # pseudocode
-- define class Cities
-- add initialize method with attributes for @name, @population, @country. These attributes should
-- exist as an array value for the hash containing all city keys.
-- add method that lists cities alphabetically
-  -use .SORT to arrange array alphabetically, no input needed
-- add method that adds city to list of cities
-- input is new city, use << to put city into cities hash with a default array of [0,0,0].
+# - define class City_creator
+# - add initialize method with attributes for @name, @population, @country. These attributes should exist as an array value for the hash containing all city keys.
+# - add method that lists cities alphabetically
+#   -use .SORT to arrange array alphabetically, no input needed
+# - add method that adds city to list of cities
+# - input is new city, use << to put city into cities hash with a default array of [0,0,0].
 
-class Cities
+class City_creator
+
+  attr_reader :name
+  attr_accessor :population, :country
+
+  def initialize(name)
+    continents = ["Asia", "North America", "South America", "Oceania", "Africa", "Europe", "Antarctica"]
+    @name = name
+    @population = rand(1...100) * 100000
+    @continent = continents[rand(0...6)]
+  end
+
+  def add_population(population)
+    @population = population
+  end
+
+  def add_country(country)
+    @country = country
+  end
 
 end
+
+all_cities = []
+
+all_cities = City_creator.new("Milwaukee")
+# all_cities.add_population("600,000")
+
+p all_cities
