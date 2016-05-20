d0d4c4cbcb44068875b4ae6bb6471a4fc7d6d9cf
abb1991 <alexbills1991@gmail.com>
2016-05-20 15:28:28 -0500

    initial commit, added pseudocode

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,1 +1,12 @@
-hello world
+# pseudocode
+- define class Cities
+- add initialize method with attributes for @name, @population, @country. These attributes should
+- exist as an array value for the hash containing all city keys.
+- add method that lists cities alphabetically
+  -use .SORT to arrange array alphabetically, no input needed
+- add method that adds city to list of cities
+- input is new city, use << to put city into cities hash with a default array of [0,0,0].
+
+class Cities
+
+end
