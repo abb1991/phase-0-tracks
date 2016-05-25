d13c8e0c423227f1957387f38e658f84d12570d2
abb1991 <alexbills1991@gmail.com>
2016-05-25 10:11:05 -0700

    initial commit, added pseudocode

---
+++ js/explore.js
@@ -0,0 +1,3 @@
+// def a var string.
+// construct a for loop for the variable string. The for loop should iterate through the string and return each letter from the end of the string to a new variable, varBackwars, that concatenates the strings together into the backwards form of the string.
+// Then print, console.log, varBackwards.
