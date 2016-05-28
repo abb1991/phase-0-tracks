467d2bb4a5c8ef28888bf5304b666a5d6988122d
abb1991 <alexbills1991@gmail.com>
2016-05-28 08:52:55 -0700

    initial commit, added pseudo code and defined initial function to check length of input

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// pseudocode
+// Construct method that takes an array of words and return the phrase with the greatest number of characters.
+// Iterate through the array, calling the .length method on each item so the function's return is the integer for how many characters are in the array. Create an if statement that checks wether or not the length of a phrase is greater than less than or equal to the previous longest phrase, which would be stored in a variable inside the if statement (with a defaul value of 0). If it's greater, than the variable's value is changed to the new greatest integer and phrase is stored in another variable as the longest phrase. Once the entire array has been iterated through, the function returns the values of the integer and the longest phrase.
+
+var arr = ["short", "longer", "longest", "even longer"]
+
+function length_checker(str){
+  console.log(str.length)
+}
+
+length_checker(arr)
